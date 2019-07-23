0bcf7cfd9b73ab12b18f8bbd64d75b656812ada4
Yingqiang Ge <gyq1101@gmail.com>
2019-07-23 01:57:03 -0400

    add spu_dict

--- examples/rec_es/rec_input_fn_local.py
+++ examples/rec_es/rec_input_fn_local.py
@@ -13,7 +13,7 @@ def _parse_dense_features(s, dshape, dtype=tf.float32, delimiter=','):
     value = tf.cast(value, dtype)
     return tf.reshape(value, dshape)
 
-def _parse_dense_int_features(s, dshape, dtype=tf.int32, delimiter=','):
+def _parse_dense_int_features(s, dshape, dtype=tf.int64, delimiter=','):
     record_defaults = [[0.0]] * dshape[1]
     #过decode_csv输出一个list of tensors，长度为50，每个item是一个float32类型的batch data
     value = tf.decode_csv(s, record_defaults=record_defaults, field_delim=delimiter)
@@ -60,10 +60,11 @@ def input_fn(name="input", tables="", num_epochs=None, num_workers=1, worker_id=
             # price = data[9] #type: string
             # isclick = data[10] #type: string
             # pay = data[11] #type: string
-            c = np.random.random([10,1]) 
+            # c = np.random.random([10,1]) 
             # pageid = tf.nn.embedding_lookup(c, data[2])  
 
             pageid = data[2] #type: int32
+            itemid = data[3]
             ctr = data[8] #type: string
             cvr = data[9] #type: string
             price = data[10] #type: string
@@ -71,17 +72,22 @@ def input_fn(name="input", tables="", num_epochs=None, num_workers=1, worker_id=
             pay = data[34] #type: string
             
             #ctr,cvr等实际上是string，里面每行包含了50个item，需要通过_parse_dense_features转成float形式
+            itemid = _parse_dense_int_features(itemid,(-1,50))
+            # for i in itemid:
+                
             ctr = _parse_dense_features(ctr, (-1, 50)) #batch_size x 50
             cvr = _parse_dense_features(cvr, (-1, 50))
             price = _parse_dense_features(price, (-1, 50))
-            # isclick = _parse_dense_int_features(isclick, (-1, 50))
-            # isclick = _parse_dense_features(isclick, (-1, 50))
-            isclick = tf.cast(tf.nn.embedding_lookup(c, _parse_dense_int_features(isclick, (-1, 50))),tf.float32)
-            isclick = tf.reshape(isclick,(-1,50))
+            isclick = _parse_dense_features(isclick, (-1, 50))
+
+            # isclick = tf.cast(tf.nn.embedding_lookup(c, _parse_dense_int_features(isclick, (-1, 50))),tf.float32)
+            # isclick = tf.reshape(isclick,(-1,50))
+
             pay = _parse_dense_features(pay, (-1, 50))
             # print(ctr)
 
             batch_data = {'keys': batch_keys,
+                        'itemid':itemid,
                         'pageid': pageid,
                         'ctr': ctr,
                         'cvr': cvr,
@@ -106,7 +112,7 @@ if __name__ == '__main__':
                 num_workers=1,
                 worker_id=0,
                 capacity=10000,
-                batch_size=100
+                batch_size=1
                 
             )
     
@@ -119,8 +125,8 @@ if __name__ == '__main__':
         try:
             while not coord.should_stop():
                 sess.run(batch_data)
-                # print(batch_data['click'].eval())
-                print(batch_data['click'].shape)
+                print(batch_data['itemid'].eval())
+                # print(batch_data['itemid'].shape)
         #当文件队列读到末尾的时候，抛出异常
         except tf.errors.OutOfRangeError:
             print('done')
